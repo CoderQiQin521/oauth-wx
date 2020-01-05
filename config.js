53f7c3bb1999a974118b015f05e5b4c0ffa666b4
CoderQiQin521 <coderqiqin@aliyun.com>
2020-01-05 13:05:41 +0800

    demo

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  appid: 'wx3a05113f324ee4a3',
+  appsecret: 'acd6266fb8111811e9c4cef102488761'
+}
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const config = require('./config')
+
+
+app.use(require('cors')())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+app.get('/user', (req, res) => {
+  res.sendFile(__dirname + '/user.html')
+})
+
+/* ---------------------------------- 后端逻辑 ---------------------------------- */
+
+app.get('/auth', (req, res) => {
+  // 后端重定向(总是跨域) 或者  url 返回前端跳转
+  let urls = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + config.appid + '&redirect_uri=http://server.utools.club/token&response_type=code&scope=snsapi_userinfo&state=http://server.utools.club/user#wechat_redirect'
+  // res.send(urls);
+  res.redirect(301, urls)
+})
+
+
+app.get('/token', (req, res) => {
+  let { code, state } = req.query
+  let accessUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${config.appid}&secret=${config.appsecret}&code=${code}&grant_type=authorization_code`
+
+  axios.get(accessUrl).then(token => {
+    let { access_token, opendid } = token.data
+
+    axios.get(`https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${opendid}&lang=zh_CN`).then(userinfo => {
+      console.log('userinfo: ', userinfo.data);
+      // state += '?token=' + Date.now()
+      state += '?userinfo=' + encodeURIComponent(JSON.stringify(userinfo.data))
+      // console.log('state: ', state);
+      res.redirect(301, state)
+    })
+
+  })
+
+})
+
+var server = app.listen(3000, 'localhost', function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('服务已启动, http://%s:%s', host, port);
+})
